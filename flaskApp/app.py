93ce8822ed1a7f4969eccee45113115bb716b2fc
AayushiPastagia <53651953+AayushiPastagia@users.noreply.github.com>
2021-05-07 13:09:51 +0530

    webApp

---
+++ flaskApp/app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask('__name__')
+
+model = pickle.load(open('./notebook/model.model.pkl','rb'))
+
+@app.route('/')
+def loadPage():
+    return render_template('home.html', query = "")
+
+@app.route('/', methods = ['POST'])
+def predict():
+    inputQuery1 = request.form['query1']
+    inputQuery2 = request.form['query2']
+    inputQuery3 = request.form['query3']
+    inputQuery4 = request.form['query4']
+    inputQuery5 = request.form['query5']
+    inputQuery6 = request.form['query6']
+    inputQuery7 = request.form['query7']
+    inputQuery8 = request.form['query8']
+    inputQuery9 = request.form['query9']
+    inputQuery10 = request.form['query10']
+    inputQuery11 = request.form['query11']
+    inputQuery12 = request.form['query12']
+    inputQuery13 = request.form['query13']
+    inputQuery14 = request.form['query14']
+    inputQuery15 = request.form['query15']
+    inputQuery16 = request.form['query16']
+    inputQuery17 = request.form['query17']
+    inputQuery18 = request.form['query18']
+    inputQuery19 = request.form['query19']
